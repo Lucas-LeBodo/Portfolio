c739aa0e26dc433ab1118fcb6660ee3f903eee1f
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-07-13 22:44:23 +0200

    create skill.jsx file

---
+++ portfolio/src/Components/skills.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { GoMarkGithub } from "react-icons/go";
+import {FaLinkedin} from "react-icons/fa";
+import gsap from 'gsap';
+
+
+// Images 
+import me from '../img/lucas-epitech.jpg';
+
+
+function SkillComponents() {
+
+    useEffect(() => {
+        gsap.from("#about", {x: "-150%", duration:1.7, opacity:1});
+    }, []);
+    return(
+        <Fragment>
+            <div id={'about'}>
+                <div id={'about-picture'}><img src={me} alt="me" /></div>
+                <div id={'about-info'}>
+                    <h1>Lucas Le Bodo</h1>
+                    <h2>Intégrateur/Développeur web Fullstack</h2>
+                    <p>Je suis <span id={'color'}>Lucas Le Bodo</span> et j'ai 19ans.<br />
+                       Actuellement étudiant à la <a href="https://www.webacademie.org/">Web@cademie</a> de Nantes, je suis une formation sur 2 ans pour devenir Intégrateur/Développeur web fullStack.<br /><br />
+                       Je recherche actuellement une alternance à partir du 27 septembre 2021 au 18 novembre 2022.
+                    </p>
+                </div>
+                <div id={'about-button'}>
+                    <div className={'github-btn'}>
+                        <a href="https://github.com/Lucas-LeBodo"><GoMarkGithub/> GitHub</a>
+                    </div>
+                    <div className={'linkedin-btn'}>
+                        <a href="https://www.linkedin.com/in/lucas-le-bodo/"><FaLinkedin/> Linkedin</a>
+                    </div>
+                </div>
+            </div>
+
+        </Fragment>
+    );
+}
+
+export default SkillComponents;
