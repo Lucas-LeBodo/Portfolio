7a326706ec9f9c7382c3304e4bf93d98ae62e148
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-08-30 15:20:34 +0200

    Create box

---
+++ client/src/Components/Elements/Cadre/box.jsx
@@ -0,0 +1,18 @@
+// Import Libs 
+import React, { Fragment } from "react";
+
+import Info from './info';
+
+const Box = ( ) => {
+
+    return (
+        <Fragment>
+            <div className="box">
+                <div className="content">
+                    <Info/>
+                </div>
+            </div>
+        </Fragment>
+    )
+} 
+export default Box;
