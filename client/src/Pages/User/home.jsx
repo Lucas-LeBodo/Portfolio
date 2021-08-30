d9e8057dd256f5cdc2a49355bf972da2551d6a18
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-08-30 11:54:03 +0200

    clear home page

--- client/src/Pages/User/home.jsx
+++ client/src/Pages/User/home.jsx
@@ -2,13 +2,13 @@
 import React, { Fragment } from 'react'
 
 // Import Components
-import AddButton from '../../Components/Elements/buttons/add';
+// import AddButton from '../../Components/Elements/buttons/add';
 
 const Home = () => {
 
     return (
         <Fragment>
-            <AddButton name="Test"/>
+            
         </Fragment>
     )
 }
