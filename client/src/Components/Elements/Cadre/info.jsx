21cb84ded493d17a260d81a9ce60d94d36e93d14
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-08-30 15:20:58 +0200

    Write first text impression

---
+++ client/src/Components/Elements/Cadre/info.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+
+const Info = ( ) => {
+
+    return (
+        <Fragment>
+            <div className="cadre-info">
+                <h3>
+                    Je suis<br/>
+                    <span className="colored">Lucas Le Bodo</span>
+                </h3>
+                <p>
+                    Étudiant : Intégrateur / Développeur Web FullStack <br />
+                    sur Nantes
+                </p>
+            </div>
+        </Fragment>
+    )
+} 
+export default Info;
